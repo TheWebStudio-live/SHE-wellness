2c0cc59ddc4fab448f11c61c30440bf7c4b9c75d
TheWebStudio-live <thewebstudio.live@gmail.com>
2025-09-25 06:00:23 -0400

    Update PublicSpeakingEngagements.tsx

--- client/src/pages/PublicSpeakingEngagements.tsx
+++ client/src/pages/PublicSpeakingEngagements.tsx
@@ -9,7 +9,7 @@ import speakingImage from '@assets/generated_images/Public_speaking_empowerment_
 const upcomingEvents = [
   "March 1st-3rd, 2026 - Women's Leadership and Empowerment Conference, Bangkok, Thailand",
   // Add more events below (up to 5 total):
-  // "Date - Event Name, Location",
+  // "Test additional event",
   // "Date - Event Name, Location",
   // "Date - Event Name, Location",
   // "Date - Event Name, Location"
