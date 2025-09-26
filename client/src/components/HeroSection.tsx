b3b2fe7d91387d04f0ca2f962a925485f062d5e8
itsrachelle <45097620-itsrachelle@users.noreply.replit.com>
2025-09-26 17:44:22 +0000

    Restored to '4499bf9628cec5ce8769c86cb595a4ff26c7e509'
    Replit-Restored-To: 4499bf9628cec5ce8769c86cb595a4ff26c7e509

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -7,16 +7,11 @@ export default function HeroSection() {
       <style>
         {`
           .hero-background {
-            background-size: contain;
-            background-position: right center;
-            background-repeat: no-repeat;
-            background-color: #E5EFE6;
+            background-position: 75% center;
           }
           @media (min-width: 768px) {
             .hero-background {
-              background-size: cover;
               background-position: center;
-              background-color: transparent;
             }
           }
         `}
